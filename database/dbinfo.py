49df051853657a0803f4ba4848fbf04dc2f7b7da
hansel-3 <hansel.oduah@ucdconnect.ie>
2026-04-30 16:48:07 +0100

    Update dbinfo.py

--- database/dbinfo.py
+++ database/dbinfo.py
@@ -4,11 +4,11 @@ except ImportError:
     pass
 
 STATIONS_URL = "https://api.jcdecaux.com/vls/v1/stations"
-API_KEY = "3fc6b50e1866c9ea8676cd4ad22a5a6576bf2c23"
+API_KEY = ""
 CONTRACT = "dublin"
 
 OPENWEATHER_URL = "https://api.openweathermap.org/data/2.5/weather"
 OPENWEATHER_FORECAST_URL = "https://api.openweathermap.org/data/2.5/forecast"
-OPENWEATHER_KEY = "0c8c3586bebde3b81f2c988dafba1a52"
+OPENWEATHER_KEY = ""
 LAT = 53.3497
 LON = -6.2603
