97863386e7cd4668d60fe1b114f2fee337b09a57
Hansel <hansel.oduah@ucdconnect.ie>
2026-05-01 08:50:11 +0100

    Update files

--- static/javascript/infoWindow.js
+++ static/javascript/infoWindow.js
@@ -1,41 +1,29 @@
-import { myMap } from "./index.js";
-import { displayDetailedBikes} from "./displayBikes.js";
+import { my_map } from "./index.js";
+import { displayDetailedBikes} from "./display_bikes.js";
 
-// define infoWindow variable
 const infoWindow = new google.maps.InfoWindow();
+  
+// function to display infoWindow
+export function displayInfw(dyn, station){
 
-/**
- * Displays InfoWindow popup
- * 
- *    Sets content for infoWindow 
- *    Calls displayDetailedBikes() to display station information board side panel
- * 
- * @param {Object} currentData Live information for selected station
- * @param {Object} staticData Static information for selected station
- */
-export function displayInfoWindow(currentData, staticData){
-  // define content for infoWindow
-  const content = `
+  let content = `
         <div class="infw_div">
-          <p class="infw_title">Station ${staticData.number}: ${staticData.name}</p>
+          <p class="infw_title">Station ${station.number}: ${station.name}</p>
           <div class="bike_summary">
-              <p class="infw_status"> ${currentData.status}</p>
-              <p class="infw_count">${currentData.available_bikes}<i class="fa-solid fa-bicycle"></i></p>
-              <p class="infw_count">${currentData.available_stands}<i class="fa-solid fa-square-parking"></i></p>
+              <p class="infw_status"> ${dyn.status}</p>
+              <p class="infw_count">${dyn.available_bikes}<i class="fa-solid fa-bicycle"></i></p>
+              <p class="infw_count">${dyn.available_stands}<i class="fa-solid fa-square-parking"></i></p>
           </div>
           <button class="more_info_btn">More Information</button> 
         </div>
       `;
-
-  // wait for button to exist in DOM before applying its onclick function - opens detailed station information
   infoWindow.addListener("domready", () => {
-        const button = document.querySelector(".more_info_btn");
-        if(button) {
-            button.onclick = () => displayDetailedBikes(currentData, staticData);
+        const btn = document.querySelector(".more_info_btn");
+        if(btn) {
+            btn.onclick = () => displayDetailedBikes(dyn, station);
         };
       });
-  // set content and position of infoWindow
   infoWindow.setContent(content);
-  infoWindow.setPosition({lat: staticData.lat, lng: staticData.lng});
-  infoWindow.open(myMap);
+  infoWindow.setPosition({lat: station.lat, lng: station.lng});
+  infoWindow.open(my_map);
 }
